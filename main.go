a64a5c47a8b1d61baa9d7dbcb80fd309cc68bd64
KJone <58522694+KJone1@users.noreply.github.com>
2022-11-02 22:59:14 +0200

    Removed bufio scanner in favor of new approach.

--- main.go
+++ main.go
@@ -2,49 +2,62 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
 )
 
 func main() {
+	q := []string{}
+	a := []string{}
 	correct_answers := 0
+
 	csvFileName := flag.String("c", "questions.csv", "csv file or whatever")
 	flag.Parse()
 
-	file, err := os.Open(*csvFileName)
+	file, err := os.ReadFile(*csvFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 	fmt.Printf("Questions from: %s\n", *csvFileName)
-	scanner := csv.NewReader(file)
+	newline_separated := strings.Split(string(file), "\r\n")
+	for index := range newline_separated {
+		comma_separated := strings.Split(newline_separated[index], ",")
+		q = append(q, comma_separated[0])
+		a = append(a, comma_separated[1])
+	}
 
-	for {
-		scanned, err := scanner.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Question : %s\n", scanned[0])
+	for index, scanned := range q {
+
+		fmt.Printf("Question : %s\n", scanned)
 		fmt.Print("Your answer: ")
-		reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		userin, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		ans := strings.TrimRight(reader, "\r\n")
-		if ans == scanned[1] {
+		user_answer := strings.TrimRight(userin, "\r\n")
+		if user_answer == a[index] {
 			correct_answers++
-			fmt.Printf("Good job %s is the correct answer. \n-\n", ans)
+			fmt.Fprintf(os.Stdout, "\033[32mGood job %s is the correct answer. \n-\n\033[0m", user_answer)
 		} else {
-			fmt.Printf("Try again.\n-\n")
+			for {
+				fmt.Fprintf(os.Stdout, "\033[31mWrong Answer try again.\n-\n\033[0m")
+				fmt.Printf("Question : %s\n", scanned)
+				fmt.Print("Your answer: ")
+				userin, err := bufio.NewReader(os.Stdin).ReadString('\n')
+				if err != nil {
+					log.Fatal(err)
+				}
+				user_answer := strings.TrimRight(userin, "\r\n")
+				if user_answer == a[index] {
+					correct_answers++
+					fmt.Fprintf(os.Stdout, "\033[32mGood job %s is the correct answer. \n-\n\033[0m", user_answer)
+					break
+				}
+			}
 		}
 	}
-	defer fmt.Printf("Game Ended. \nNumber of correct answers: %d.\n", correct_answers)
+	fmt.Printf("Game Ended. \nNumber of correct answers: %d.\n", correct_answers)
 }
